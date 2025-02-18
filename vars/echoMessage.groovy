7b0ccfb8432112fe9efd4a7235ae3ee11b69c0c8
Bhargav-manepalli <118068077+Bhargav-manepalli@users.noreply.github.com>
2025-02-18 05:55:52 +0000

    Update echoMessage.groovy

--- vars/echoMessage.groovy
+++ vars/echoMessage.groovy
@@ -1,6 +1,6 @@
-def call(int maxCount = 19) {
-    for (int i = 0; i < maxCount; i++) {
-        echo "Echo message #${i + 1}"
-    }
+def call() {
+    echo "Echo message #1"
+    echo "Echo message #2"
+    echo "Echo message #3"
 }
 
